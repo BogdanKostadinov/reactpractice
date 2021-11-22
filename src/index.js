c33136dafd460df9a281fdc48dbf5026ca660e72
Bogdan Kostadinov <bk7907k@gre.ac.uk>
2021-11-22 22:49:28 +0000

    testing stuff

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+const JSX = (<div>
+  <p>Hello</p>
+  <p>Worlld</p>
+</div>);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
   document.getElementById('root')
 );
